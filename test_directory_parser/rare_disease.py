4fc68e56998fbc0e415dd66d86422f6c47ad7c12
Yujin Kim <yujin.kim@hotmail.fr>
2022-07-05 14:50:39 +0100

    docstrings

--- test_directory_parser/rare_disease.py
+++ test_directory_parser/rare_disease.py
@@ -1,10 +1,18 @@
 import json
 
 import pandas as pd
-from termcolor import colored, cprint
 
 
 def parse_config(config):
+    """ Parse config file
+
+    Args:
+        config (str): Path to the config file
+
+    Returns:
+        dict: Dict of config data
+    """
+
     config_content = open(config)
     config_data = json.load(config_content)
     config_content.close()
@@ -12,6 +20,16 @@ def parse_config(config):
 
 
 def parse_rare_disease_td(test_directory, config):
+    """Parse rare disease test directory using the config file
+
+    Args:
+        test_directory (str): Path to the test directory
+        config (dict): Dict containing the data for the config file
+
+    Returns:
+        pandas.Dataframe: Dataframe containing the columns of interest
+    """
+
     xls = pd.read_excel(
         test_directory, sheet_name=None, header=config["header_index"]
     )
