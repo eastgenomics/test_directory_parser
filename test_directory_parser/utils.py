3195fe2c08ba4c949cb12f6611931833a8840c6a
Yujin Kim <yujin.kim@hotmail.fr>
2022-07-06 10:33:54 +0100

    simplify code + handle non matching gene symbols

--- test_directory_parser/utils.py
+++ test_directory_parser/utils.py
@@ -12,9 +12,7 @@ def get_date():
         str: Datetime in string format
     """
 
-    now = datetime.datetime.now()
-    date = now.strftime("%y%m%d")
-    return date
+    return datetime.datetime.now().strftime("%y%m%d")
 
 
 def parse_tsv(tsv):
@@ -49,14 +47,14 @@ def find_hgnc_id(gene_symbol, hgnc_dump):
 
     # match failed, need to use previous and alias symbols
     if len(data.index) == 0:
-        if match_target(r"[A-Z]+[A-Z0-9]+", gene_symbol):
+        if regex.match(r"[A-Z]+[A-Z0-9]+", gene_symbol):
             # previous and alias symbols in hgnc are separated by commas which
             # breaks the pattern matching so i split the appropriate columns
             # creating a df with one column per splitted element
             splitted_previous_symbols = previous_symbols["Previous symbols"].str.split(",", expand=True)
             splitted_alias_symbols = alias_symbols["Alias symbols"].str.split(",", expand=True)
             # copy pasted thing to do the pattern matching for every element of
-            # the splitted column 
+            # the splitted column
             previous_symbols_match = np.column_stack(
                 [
                     splitted_previous_symbols[col].str.strip().str.match(pattern, na=False)
@@ -91,25 +89,6 @@ def find_hgnc_id(gene_symbol, hgnc_dump):
         return [hgnc_id for hgnc_id in data][0]
 
 
-def match_target(pattern, target):
-    """ Regex match pattern to the target
-
-    Args:
-        pattern (regex str): Pattern to match to the target
-        target (str): Target
-
-    Returns:
-        bool: if pattern matched
-    """
-
-    match = regex.match(pattern, target)
-
-    if match:
-        return True
-    else:
-        return False
-
-
 def handle_list_panels(panels, hgnc_dump):
     """ Given a list of "panels", get the hgnc ids/rescue comma panelapp panels
 
@@ -126,31 +105,36 @@ def handle_list_panels(panels, hgnc_dump):
     # check if the list only contains genes
     # check that the first element has a gene structure i.e. it's
     # not something weird
-    if match_target(r"[A-Z]+[A-Z0-9]+", panels[0]):
+    if regex.match(r"[A-Z]+[A-Z0-9]+", panels[0]):
         # we are dealing with genes
         for panel in panels:
-            if match_target(r"[A-Z]+[A-Z0-9]+", panel):
+            if regex.match(r"[A-Z]+[A-Z0-9]+", panel):
                 hgnc_id = find_hgnc_id(panel, hgnc_dump)
 
                 if hgnc_id:
                     hgnc_ids.append(hgnc_id)
                 else:
-                    attempt_rescue_gene = [
-                        gene.strip() for gene in panel.split("and")
-                    ]
+                    if "and" in panel:
+                        attempt_rescue_gene = [
+                            gene.strip() for gene in panel.split("and")
+                        ]
 
-                    # check if there were 2 genes from each side of the 'and'
-                    if len(attempt_rescue_gene) >= 2:
-                        for gene in attempt_rescue_gene:
-                            rescued_gene = find_hgnc_id(gene, hgnc_dump)
+                        # check if there were 2 genes from each side of the 'and'
+                        if len(attempt_rescue_gene) >= 2:
+                            for gene in attempt_rescue_gene:
+                                rescued_gene = find_hgnc_id(gene, hgnc_dump)
 
-                            if rescued_gene:
-                                hgnc_ids.append(rescued_gene)
+                                if rescued_gene:
+                                    hgnc_ids.append(rescued_gene)
+                                else:
+                                    hgnc_ids.append(None)
+                    else:
+                        hgnc_ids.append(None)
 
         return hgnc_ids
     else:
         # some panelapp panels have commas
-        if match_target(r"[A-Za-z0-9-()\ ,]*\([0-9&\ ]+\)", ", ".join(panels)):
+        if regex.match(r"[A-Za-z0-9-()\ ,]*\([0-9&\ ]+\)", ", ".join(panels)):
             return [", ".join(panels)]
 
         return None
