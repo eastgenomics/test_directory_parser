38b611e5ffcc174131f7a01302597f982f55ff96
Yujin Kim <yujin.kim@hotmail.fr>
2023-10-24 12:06:49 +0100

    more documentation

--- test_directory_parser/clinical_indication.py
+++ test_directory_parser/clinical_indication.py
@@ -1,10 +1,26 @@
+import pandas as pd
 import regex
 
 from test_directory_parser import utils
 
 
 class ClinicalIndication:
-    def __init__(self, r_code, name, panels, test_method, change, hgnc_dump):
+    def __init__(
+        self, r_code: str, name: str, target: str, test_method: str,
+        change: str, hgnc_dump: pd.DataFrame
+    ):
+        """ Setup the clinical indication object
+
+        Args:
+            r_code (str): R code associated with the clinical indication
+            name (str): Name of the clinical indication
+            target (str): Target of the clinical indication
+            test_method (str): Test method of the clinical indication
+            change (str): Changes described in the test directory excel i.e.
+            no changes, addition of, removal of ...
+            hgnc_dump (pd.DataFrame): Dataframe containing the HGNC data
+        """
+
         self.r_code = r_code
         # R424 has a space at the end of its name
         self.name = name.replace("–", "-").strip()
@@ -14,12 +30,12 @@ class ClinicalIndication:
         else:
             self.gemini_name = f"{self.r_code}_{self.name}_P"
 
-        self.original_targets = panels
+        self.original_targets = target
         self.test_method = test_method
         self.change = change
-        self.clean_panels(hgnc_dump)
+        self.clean_target(hgnc_dump)
 
-    def clean_panels(self, hgnc_dump):
+    def clean_target(self, hgnc_dump: pd.DataFrame):
         """ Attempt to clean up the targets in the excel file
 
         Args:
