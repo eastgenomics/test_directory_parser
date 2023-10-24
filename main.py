3dbae198ed059f70fab6e3480a5c4a73d52f9c24
Yujin Kim <yujin.kim@hotmail.fr>
2023-10-24 11:26:34 +0100

    remove the output checker things as they are now in the td checker

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import argparse
-import json
-
-from panelapp import queries
 
 from test_directory_parser import utils
 from test_directory_parser import test_directory
-from test_directory_parser import output_checker
 
 
 def main(args):
@@ -33,42 +29,6 @@ def main(args):
     elif cmd == "cancer":
         print("Parsing of the cancer test directory is not implemented yet")
 
-    elif cmd == "checker":
-        session, meta = utils.connect_to_local_database(
-            args.username, args.passwd, args.database_name
-        )
-        td_parser_output = json.load(open(args.td_parser_output))
-
-        if args.filter:
-            cis = output_checker.get_clinical_indications(
-                td_parser_output, args.filter
-            )
-            output_checker.write_data(
-                cis, f"clinical_indication_with_{args.filter}.txt"
-            )
-
-        panelapp_panels = queries.get_all_signedoff_panels()
-
-        (
-            absent_genes_per_panel, no_clinical_transcripts_per_panel,
-            absent_genes, genes_no_clinical_transcripts
-        ) = output_checker.compare_panelapp_panels_content(
-            session, meta, td_parser_output, panelapp_panels
-        )
-
-        output_checker.write_data(
-            absent_genes_per_panel, "absent_genes_per_clinical_indication.txt"
-        )
-        output_checker.write_data(
-            no_clinical_transcripts_per_panel,
-            "no_clinical_transcripts_per_clinical_indication.txt"
-        )
-
-        output_checker.write_data(absent_genes, "genes_not_in_database.txt")
-        output_checker.write_data(
-            genes_no_clinical_transcripts,
-            "genes_with_no_clinical_transcripts_in_database.txt"
-        )
     else:
         raise Exception(f"'{cmd}' is not a valid option")
 
@@ -93,24 +53,6 @@ if __name__ == "__main__":
     cancer_parser = subparsers.add_parser("cancer")
     cancer_parser.add_argument("test_directory", help="Path to test directory")
     cancer_parser.set_defaults(which="cancer")
-
-    checker_parser = subparsers.add_parser("checker")
-    checker_parser.add_argument(
-        "username", help="Username for the panel database"
-    )
-    checker_parser.add_argument(
-        "passwd", help="Password for given username"
-    )
-    checker_parser.add_argument(
-        "database_name", help="Name of the panel database"
-    )
-    checker_parser.add_argument(
-        "td_parser_output", help="Json output of the test directory parser"
-    )
-    checker_parser.add_argument(
-        "-f", "--filter", help="Filter option on the change column"
-    )
-    checker_parser.set_defaults(which="checker")
 
     parser.add_argument(
         "-c", "--config",
