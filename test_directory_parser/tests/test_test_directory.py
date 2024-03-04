0d402993745565115cb90ae2ed732a716a9e9bb1
Yujin Kim <yujin.kim@hotmail.fr>
2024-03-04 16:03:23 +0000

    setupclass + filter ci internal

--- test_directory_parser/tests/test_test_directory.py
+++ test_directory_parser/tests/test_test_directory.py
@@ -3,45 +3,20 @@ from unittest.mock import patch
 
 import pandas as pd
 
-from test_directory_parser.clinical_indication import ClinicalIndication
 from test_directory_parser.test_directory import TestDirectory
 
 class TestTestDirectory(unittest.TestCase):
     """ Test the methods of the test directory object """
 
-    @patch("test_directory_parser.test_directory.rare_disease.parse_config")
-    @patch("test_directory_parser.test_directory.rare_disease.parse_rare_disease_td")
-    @patch("test_directory_parser.test_directory.clinical_indication.ClinicalIndication")
-    def test_setup_clinical_indications(
-        self, mock_config_data, mock_td_data, mock_clinical_indication
-    ):
+    @classmethod
+    @patch("test_directory_parser.rare_disease.parse_rare_disease_td")
+    @patch("test_directory_parser.rare_disease.parse_config")
+    def setUpClass(cls, mock_config_data, mock_td_data):
         """ Test the setup clinical indications method. The config data had to
         be mocked by setting the config variable rather than mocking the
         rare_disease.parse_config function """
-        mock_config_data.return_value = {}
 
-        mock_td_data.return_value = (
-            pd.DataFrame(
-                {
-                    "Test ID": ["R100.1", "R200.1"],
-                    "Clinical Indication": ["CI1", "CI2"],
-                    "Target/Genes": ["Panel1", "Panel2"],
-                    "Test Method": ["WES", "Not WES"],
-                    "Specialist test group": ["Core", "Core"],
-                    "Changes since April 2023 publication": ["No change", "No change"]
-                }
-            ),
-            "Changes since April 2023 publication"
-        )
-
-        mock_clinical_indication.return_value = ClinicalIndication(
-            "R100.1", "CI1", "Panel1", "WES", "Change", ""
-        )
-
-        td = TestDirectory("", "", "rare_disease", "")
-        # assign the config variable here because mocking the value before that
-        # was causing issues
-        td.config = {
+        mock_config_data.return_value = {
             "name": "Test_config",
             "sheet_of_interest": "R&ID indications",
             "clinical_indication_column_code": "Test ID",
@@ -62,13 +37,52 @@ class TestTestDirectory(unittest.TestCase):
                 "WGS", "WES", "Exon level CNV detection by MLPA or equivalent"
             ]
         }
-        td.setup_clinical_indications()
 
+        mock_td_data.return_value = (
+            pd.DataFrame(
+                {
+                    "Test ID": ["R100.1", "R200.1"],
+                    "Clinical Indication": ["CI1", "CI2"],
+                    "Target/Genes": ["Panel1", "Panel2"],
+                    "Test Method": ["WES", "Not WES"],
+                    "Specialist test group": ["Core", "Core"],
+                    "Changes since April 2023 publication": ["No change", "No change"]
+                }
+            ),
+            "Changes since April 2023 publication"
+        )
+
+        td = TestDirectory("", "", "rare_disease", "")
+        td.setup_clinical_indications()
+        cls.td = td
+
+    def test_setup_clinical_indications(self):
         # the clinical indications variables are lists containing clinical
         # indication objects and I don't have ways to compare those objects
         # instead, I check the number of objects in each lists
         with self.subTest():
-            self.assertEqual(len(td.ngs_clinical_indications), 1)
+            self.assertEqual(len(self.td.ngs_clinical_indications), 1)
 
         with self.subTest():
-            self.assertEqual(len(td.all_clinical_indications), 2)
+            self.assertEqual(len(self.td.all_clinical_indications), 2)
+
+    def test_filter_clinical_indications_present(self):
+        test_internal_td = pd.DataFrame(
+            {
+                "Clinical indication ID": ["R100"],
+                "Test ID": ["R100.1"],
+                "Clinical Indication": ["CI1"],
+                "Target/Genes": ["Panel1"],
+                "Test Method": ["WES"],
+                "Commissioning category": ["Category1"],
+                "Specialist test group": ["Core"],
+                "Changes since April 2023 publication": ["No change"]
+            }
+        )
+
+        self.td.filter_clinical_indications(test_internal_td)
+
+        self.assertEqual(len(self.td.filtered_clinical_indications), 1)
+
+    def test_filter_clinical_indications_absent(self):
+        pass
