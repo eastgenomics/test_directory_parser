6ee57501b33d8f4b6ac35406a18a86481e0e9333
Yujin Kim <yujin.kim@hotmail.fr>
2024-03-05 09:41:58 +0000

    further testing to fully cover rare_disease

--- test_directory_parser/tests/test_rare_disease.py
+++ test_directory_parser/tests/test_rare_disease.py
@@ -13,19 +13,8 @@ PATH_TO_TEST_FOLDER = Path(".") / "test_directory_parser" / "tests" / "test_data
 class TestRareDisease(unittest.TestCase):
     """ Suite of tests for the rare_disease.py script """
 
-    def test_parse_config(self):
-        """ Test for parse_config function """
-
-        path_to_test_data = PATH_TO_TEST_FOLDER / "test.json"
-        test_output = parse_config(path_to_test_data)
-        expected_output = {"test_field": "test_value"}
-        self.assertEqual(test_output, expected_output)
-
-    @patch("test_directory_parser.rare_disease.pd.read_excel")
-    def test_parse_rare_disease_td(self, mock_td_excel):
-        """ Test for parse_rare_disease_td """
-
-        test_config = {
+    def setUp(self) -> None:
+        self.test_config = {
             "name": "Test_config",
             "sheet_of_interest": "R&ID indications",
             "clinical_indication_column_code": "Test ID",
@@ -46,6 +35,26 @@ class TestRareDisease(unittest.TestCase):
                 "WGS", "WES", "Exon level CNV detection by MLPA or equivalent"
             ]
         }
+
+    def test_parse_config(self):
+        """ Test for parse_config function """
+
+        path_to_test_data = PATH_TO_TEST_FOLDER / "test.json"
+        test_output = parse_config(path_to_test_data)
+        expected_output = {"test_field": "test_value"}
+        self.assertEqual(test_output, expected_output)
+
+    @patch("test_directory_parser.rare_disease.pd.read_excel")
+    def test_parse_rare_disease_td_correctly(self, mock_td_excel):
+        """ Test for parse_rare_disease_td 
+        
+        Setup test:
+        - Normal test directory
+
+        Expectations:
+        - Found change column
+        - Correct parsing of td_excel data
+        """
 
         mock_td_excel.return_value = {
             "R&ID indications": pd.DataFrame(
@@ -73,7 +82,9 @@ class TestRareDisease(unittest.TestCase):
             }
         )
 
-        test_output, test_change_column = parse_rare_disease_td("", test_config)
+        test_output, test_change_column = parse_rare_disease_td(
+            "", self.test_config
+        )
 
         with self.subTest():
             self.assertTrue(test_output.equals(expected_output))
@@ -81,4 +92,70 @@ class TestRareDisease(unittest.TestCase):
         with self.subTest():
             self.assertEqual(
                 test_change_column, "Changes since April 2023 publication"
+            )
+
+    @patch("test_directory_parser.rare_disease.pd.read_excel")
+    def test_parse_rare_disease_td_too_many_change_columns(
+        self, mock_td_excel
+    ):
+        """ Test for parse_rare_disease_td
+
+        Setup test:
+        - Test directory with 2 columns with "changes" in their header
+
+        Expectations:
+        - Exception raised because 2 >= columns with changes in their header 
+        """
+
+        mock_td_excel.return_value = {
+            "R&ID indications": pd.DataFrame(
+                {
+                    "Clinical indication ID": ["R100"],
+                    "Test ID": ["R100.1"],
+                    "Clinical Indication": ["CI1"],
+                    "Target/Genes": ["Panel1"],
+                    "Test Method": ["WES"],
+                    "Commissioning category": ["Category1"],
+                    "Specialist test group": ["Core"],
+                    "Changes since April 2023 publication": ["No change"],
+                    "Other changes": ["Kinda change"]
+                }
+            )
+        }
+
+        with self.assertRaises(Exception):
+            test_output, test_change_column = parse_rare_disease_td(
+                "", self.test_config
+            )
+
+    @patch("test_directory_parser.rare_disease.pd.read_excel")
+    def test_parse_rare_disease_td_no_change_column(
+        self, mock_td_excel
+    ):
+        """ Test for parse_rare_disease_td
+
+        Setup test:
+        - Test directory with 0 columns with "changes" in their header
+
+        Expectations:
+        - Exception raised because 0 columns with "changes" in their header 
+        """
+
+        mock_td_excel.return_value = {
+            "R&ID indications": pd.DataFrame(
+                {
+                    "Clinical indication ID": ["R100"],
+                    "Test ID": ["R100.1"],
+                    "Clinical Indication": ["CI1"],
+                    "Target/Genes": ["Panel1"],
+                    "Test Method": ["WES"],
+                    "Commissioning category": ["Category1"],
+                    "Specialist test group": ["Core"],
+                }
+            )
+        }
+
+        with self.assertRaises(Exception):
+            test_output, test_change_column = parse_rare_disease_td(
+                "", self.test_config
             )
