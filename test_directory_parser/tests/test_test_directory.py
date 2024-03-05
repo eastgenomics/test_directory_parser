743e79d1b6f8209194741d4cc9d1364843ced7d0
Yujin Kim <yujin.kim@hotmail.fr>
2024-03-05 09:10:20 +0000

    all the docstrings

--- test_directory_parser/tests/test_test_directory.py
+++ test_directory_parser/tests/test_test_directory.py
@@ -12,9 +12,7 @@ class TestTestDirectory(unittest.TestCase):
     @patch("test_directory_parser.rare_disease.parse_rare_disease_td")
     @patch("test_directory_parser.rare_disease.parse_config")
     def setUpClass(cls, mock_config_data, mock_td_data):
-        """ Test the setup clinical indications method. The config data had to
-        be mocked by setting the config variable rather than mocking the
-        rare_disease.parse_config function """
+        """ Test the Test directory object """
 
         mock_config_data.return_value = {
             "name": "Test_config",
@@ -57,9 +55,23 @@ class TestTestDirectory(unittest.TestCase):
         cls.td = td
 
     def tearDown(self) -> None:
+        # reset the filtered_clinical_indications attributes for further testing 
         self.td.filtered_clinical_indications = []
 
     def test_setup_clinical_indications(self):
+        """ Test for setting up clinical indications i.e. adding them to the
+        NGS clinical indications or not
+        
+        Test setup:
+        - TD contains R100.1 and R200.1
+        - R100.1 is WES
+        - R200.1 is not WES
+
+        Expectation:
+        - R100.1 in ngs_clinical_indications
+        - R100.1 and R200.1 in all_clinical_indications 
+        """
+
         # the clinical indications variables are lists containing clinical
         # indication objects and I don't have ways to compare those objects
         # instead, I check the number of objects in each lists
@@ -70,6 +82,18 @@ class TestTestDirectory(unittest.TestCase):
             self.assertEqual(len(self.td.all_clinical_indications), 2)
 
     def test_filter_clinical_indications_present(self):
+        """ Test filter clinical indications method with internal TD having a
+        clinical indication 
+
+        Test setup:
+        - TD contains R100.1 and R200.1
+        - Internal TD contains R100.1
+
+        All the tests in the internal TD are present in the "normal" TD, check
+        if they are added correctly in the filtered_clinical_indications
+        attribute
+        """
+
         test_internal_td = pd.DataFrame(
             {
                 "Clinical indication ID": ["R100"],
@@ -87,6 +111,17 @@ class TestTestDirectory(unittest.TestCase):
         self.assertEqual(len(self.td.filtered_clinical_indications), 1)
 
     def test_filter_clinical_indications_absent(self):
+        """ Test filter clinical indications method with internal TD missing a
+        clinical indication
+        
+        Test setup:
+        - TD contains R100.1 and R200.1
+        - Internal TD contains R100.1 and R100.2
+
+        The code expects the tests in the internal TD to be all present in the
+        "normal" TD triggering the error
+        """
+
         test_internal_td = pd.DataFrame(
             {
                 "Clinical indication ID": ["R100", "R100"],
